bdd68fecd5e76e80f5be69971a80e6b806170c33
Victor Okonkwo <emmyvic98@gmail.com>
2022-09-15 02:06:30 +0100

    useState on items

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Item from './Item'
 import { data } from '../data'
 
 const Card = () => {
+    const [celebrants, setCelebrants] = useState(data)
   return (
     <div className='card'>
-        <h2>{data.length} birthdays today</h2>
+        <h2>{celebrants.length} birthdays today</h2>
         {
-            data.map(celebrant => {
+            celebrants.map(celebrant => {
                 // const {id, img, name, age} = celebrant
                 // return <Item key={id} img={img} name={name} age={age}/>
                 return <Item key={celebrant.id} celebrant={celebrant}/>
             })
         }
+        <button className='btn'>Clear All</button>
     </div>
   )
 }
